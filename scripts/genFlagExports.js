3b4c5d0f8cf1dc30ed46f38b8bd8954f3009a263
SMU <gh@smuco.de>
2020-11-15 10:40:02 +0100

    Optimize and correct svgs

--- scripts/genFlagExports.js
+++ scripts/genFlagExports.js
@@ -1,20 +1,20 @@
-import fs from "fs";
-import path from "path";
-import countries from "svg-country-flags/countries.json";
+import fs from 'fs'
+import path from 'path'
+import countries from 'svg-country-flags/countries.json'
 
-const imports = Object.keys(countries).map(k => {
-  const key = k.replace("-", "_");
-  return `import flag_${key} from 'svg-country-flags/svg/${k.toLowerCase()}.svg'`;
-});
+const imports = Object.keys(countries).map((k) => {
+  const key = k.replace('-', '_')
+  return `import flag_${key} from './svgs/${k.toLowerCase()}.svg'`
+})
 
-const exports = Object.keys(countries).map(k => {
-  const key = k.replace("-", "_");
-  return `flag_${key}`;
-});
+const exports = Object.keys(countries).map((k) => {
+  const key = k.replace('-', '_')
+  return `flag_${key}`
+})
 
-const target = path.resolve(__dirname, "../src/flags.js");
+const target = path.resolve(__dirname, '../src/flags.js')
 
 fs.writeFileSync(
   target,
-  imports.join("\n") + `\nexport default {${exports.join(", ")}}`
-);
+  imports.join('\n') + `\nexport default {${exports.join(', ')}}`
+)
